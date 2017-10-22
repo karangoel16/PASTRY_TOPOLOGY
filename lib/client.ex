9eb8f489b04f8457819864212e11480139d36e1a
karangoel16 <karangoel16@yahoo.com>
2017-10-22 11:31:25 -0400

    Removed exact point of reference

--- lib/client.ex
+++ lib/client.ex
@@ -145,7 +145,7 @@ defmodule Project3.Client do
                                             {x,Map.get(elem(state,0),x)|>Atom.to_string}
                                         end)|>Map.new|>Map.merge(mer)
                                         val=Enum.max_by(Map.values(mer),fn(x)->shl(a,x)-d end)
-                                        GenServer.cast({val|>String.to_atom,Node.self()},{:route,key,nextId,val|>String.to_atom,jumps+1})
+                                        #GenServer.cast({val|>String.to_atom,Node.self()},{:route,key,nextId,val|>String.to_atom,jumps+1})
                                         Enum.map(Enum.take_random(Map.values(mer),1),fn(x)->
                                             GenServer.cast({x|>String.to_atom,Node.self()},{:route,key,nextId,x|>String.to_atom,jumps+1})
                                         end)
